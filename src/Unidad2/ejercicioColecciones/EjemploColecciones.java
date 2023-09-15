2017a75b20d92c47c519337620f9080f4371625e
USRVI-LC4 <USRVI-LC4@VIAWSE02LC0400.duoc.acad>
2023-09-14 21:53:22 -0300

    Ejercicio ejemplo arrayList

---
+++ src/Unidad2/ejercicioColecciones/EjemploColecciones.java
@@ -0,0 +1,31 @@
+
+package Unidad2.ejercicioColecciones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Profe hernan
+ */
+public class EjemploColecciones {
+    
+    public static void main(String[] args) {
+        
+        //crear un arreglo
+        ArrayList<String> listaNombres = new ArrayList<>();
+      
+        listaNombres.add("Juan");
+        listaNombres.add("Pedro");
+        listaNombres.add("Maria");
+        
+        System.out.println(listaNombres);
+        
+
+        
+        
+        
+        
+    }
+    
+}
