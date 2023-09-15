6968910e3392473a6a9ef1c9e3845d0f73f4d79c
USRVI-LC4 <USRVI-LC4@VIAWSE02LC0400.duoc.acad>
2023-09-14 22:03:36 -0300

    Ejercicio ejemplo arrayList Auto

--- src/Unidad2/ejercicioColecciones/EjemploColecciones.java
+++ src/Unidad2/ejercicioColecciones/EjemploColecciones.java
@@ -21,8 +21,29 @@ public class EjemploColecciones {
         
         System.out.println(listaNombres);
         
-
+        //for para recorrer arreglos
+        //fore + tab --> foreach
+        for (String nombre : listaNombres) {
+            System.out.println(nombre);
+        }
         
+        System.out.println("");
+        System.out.println("******ARREGLO AUTO *****");
+        
+        ArrayList<Auto> listaAutos = new ArrayList<>();
+        
+       Auto auto1 = new Auto("KIA", "SPORTAGE", 2018);
+       Auto auto2 = new Auto("Toyota","SUPRA",1999);
+       
+       listaAutos.add(auto1);
+       listaAutos.add(auto2);
+       
+        System.out.println(listaAutos);
+        
+        System.out.println("MARCAS:");
+        for (Auto auto : listaAutos) {
+            System.out.println("- "+ auto.getMarca() + " " + auto.getAnno());
+        }
         
         
         
